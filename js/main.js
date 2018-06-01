76a879dac37c058b4441889ee9897fd2380f8d34
rachael miller design <rachael@rachaelmillerdesign.com>
2018-06-01 11:43:42 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
-console.log (cardOne);
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[2];
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+if (cardsInPlay.length === 2){	
+	if (cardsInPlay[0] === cardsInPlay[1]){
+	alert("You found a match!");}
+	else {
+	alert("Sorry, try again.");
+	}
+}
